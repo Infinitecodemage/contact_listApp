f22650f9d740dd76a03f1b2cda21af0263241285
mageraj <prdRiteshRaj@gmail.com>
2023-03-12 06:37:25 +0530

    Returning the response to the server

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const port = 8000;
 
 const app = express();
 
+app.get('/', function(req, res){
+    res.send(`Cool, Iruki you have setup "Returning the response from the server".`);
+})
+
 app.listen(port, function(err){
     if(err){
         console.log('hey kariel, Error in running the server');
-    }console.log(`Yup, My Express server is running on port: ${port}`)
+    }console.log(`Yup,Hey fallen Angel, My Express server is running on port: ${port}`);
 })
